af7795520ce4ae0459439c0b2de23b9febdfb84e
Painted-J <128854258+Painted-J@users.noreply.github.com>
2024-02-28 18:59:57 -0800

    Combined merge and mergeSort into a single function, haskell

--- h_sort.hs
+++ h_sort.hs
@@ -6,12 +6,11 @@ mergeSort xs = merge (mergeSort firstHalf) (mergeSort secondHalf)
     half = length xs `div` 2
     (firstHalf, secondHalf) = splitAt half xs
 
-merge :: Ord a => [a] -> [a] -> [a]
-merge xs [] = xs
-merge [] ys = ys
-merge (x:xs) (y:ys)
-  | x <= y    = x : merge xs (y:ys)
-  | otherwise = y : merge (x:xs) ys
+    merge xs [] = xs
+    merge [] ys = ys
+    merge (x:xs) (y:ys)
+      | x <= y    = x : merge xs (y:ys)
+      | otherwise = y : merge (x:xs) ys
 
 main :: IO ()
 main = do
