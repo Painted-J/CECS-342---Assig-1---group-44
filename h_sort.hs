1f28ef07d6400133dfbcf9a0d7a133b97f0c9de4
Painted-J <128854258+Painted-J@users.noreply.github.com>
2024-02-28 18:55:05 -0800

    Added merge sort and test cases; Haskell

---
+++ h_sort.hs
@@ -0,0 +1,26 @@
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = merge (mergeSort firstHalf) (mergeSort secondHalf)
+  where
+    half = length xs `div` 2
+    (firstHalf, secondHalf) = splitAt half xs
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+  | x <= y    = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
+
+main :: IO ()
+main = do
+    putStrLn "Merge Sort test:\n"
+    putStr "[9,8,7,6,5,4,3,2,1] = "
+    print $ mergeSort [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    putStr "[5,-3,2,9,7,-20,10,1,6] = "
+    print $ mergeSort [5, -3, 2, 9, 7, -20, 10, 1, 6]
+    putStr "\"qwertyuiopasdfghjklzxcvbnm\" = "
+    print $ mergeSort "qwertyuiopasdfghjklzxcvbnm"
+    putStr "[1] = "
+    print $ mergeSort [1]
